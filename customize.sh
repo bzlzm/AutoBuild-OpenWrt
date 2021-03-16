941a59a12d76d2d8bdec7a8e7b1bc4113368b5f8
bzlzm <46715173+bzlzm@users.noreply.github.com>
2021-03-16 19:35:21 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #1. 修改IP添加网关DNS
-sed -i 's/192.168.1.1/10.0.0.5/g' openwrt/package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.0.0.5/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. 版本号里显示自己的名字
 sed -i "s/OpenWrt /Bzlzm build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settiings
